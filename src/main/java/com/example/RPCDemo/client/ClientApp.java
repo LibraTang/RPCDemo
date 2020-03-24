c72e243ffb83c1eba2618e687780027da469a0d0
LibraTang <1026960839@qq.com>
2020-03-24 15:27:46 +0800

    增加客户端请求订阅监控

--- src/main/java/com/example/RPCDemo/client/ClientApp.java
+++ src/main/java/com/example/RPCDemo/client/ClientApp.java
@@ -1,20 +1,53 @@
 package com.example.RPCDemo.client;
 
 import com.example.RPCDemo.exception.MethodNotFoundException;
+import com.example.RPCDemo.registry.DiscoveryService;
+import com.example.RPCDemo.registry.Url;
 import com.example.RPCDemo.server.Calculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ClientApp {
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientApp implements DiscoveryService {
     private static Logger log = LoggerFactory.getLogger(ClientApp.class);
+    private static final ClientApp instance = new ClientApp();
+    private static final int PORT = 9093;
+    private static Url url;
 
     public static void main(String[] args) {
         Calculator calculator = new CalculatorRemoteImpl();
+        instance.subscribe("Calculator.add");
+    }
+
+    public void subscribe(String method) {
         try {
-            int result = calculator.add(1000, 2);
-            log.info("result is {}", result);
-        } catch (MethodNotFoundException e) {
-            log.error(e.getMessage());
+            Socket socket = new Socket("127.0.0.1", PORT);
+
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+            //将订阅方法发送给注册中心
+            objectOutputStream.writeObject(method);
+            log.info("订阅方法: {}", method);
+
+            //接收注册中心的响应
+            Object response = objectInputStream.readObject();
+            log.info("response from registry: {}", response);
+
+            if(response instanceof Url)
+                url = (Url) response;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/example/RPCDemo/registry/ClientMonitor.java
+++ src/main/java/com/example/RPCDemo/registry/ClientMonitor.java
@@ -1,7 +1,63 @@
 package com.example.RPCDemo.registry;
 
-public class ClientMonitor implements Runnable {
-    public void run() {
+import com.example.RPCDemo.exception.MethodNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.rmi.registry.Registry;
+
+public class ClientMonitor implements Runnable {
+    private static final Logger log = LoggerFactory.getLogger(ClientMonitor.class);
+
+    public void run() {
+        ServerSocket listener = null;
+        try {
+            listener = new ServerSocket(9093);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            while (true) {
+                Socket socket = listener.accept();
+                try {
+                    ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+                    Object request = objectInputStream.readObject();
+                    log.info("request from client: {}", request);
+
+                    if(request instanceof String) {
+                        String method = (String) request;
+                        Url url = RegistryApp.registry.get(method);
+                        if(null == url || "".equals(url)) {
+                            objectOutputStream.writeObject("没有在注册中心找到方法: " + method);
+//                            throw new MethodNotFoundException("没有在注册中心找到方法: " + method);
+                        }
+                        else {
+                            //给客户端返回该方法的url
+                            objectOutputStream.writeObject(url);
+                        }
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } finally {
+                    socket.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                listener.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
